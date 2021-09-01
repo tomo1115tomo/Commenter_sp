3eb3b82d7be6eed2af238b04ad0baea4012a1845
tomohiro0827 <w40tomosub160.04@gmail.com>
2021-09-02 01:37:22 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     resources :users
   end
 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmls
 
   resources :users
