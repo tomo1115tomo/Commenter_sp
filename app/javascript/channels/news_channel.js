e8bf3614509886dc987d7ed2abab50ae32f2b966
tomohiro0827 <w40tomosub160.04@gmail.com>
2021-09-14 01:01:27 +0900

    commit

--- app/javascript/channels/news_channel.js
+++ app/javascript/channels/news_channel.js
@@ -13,8 +13,17 @@ consumer.subscriptions.create("NewsChannel", {
     // Called when there's incoming data on the websocket for this channel
     const news_id = "news_" + String(data['receiver_id']);
     const news_area = document.getElementById(news_id);
+    var content = "<div id='" + "recent_msg_" + data['receiver_id'] + "_" + data['sender_id'] + "'>" + data['senderid'] + " からの新着メッセージがあります" + "</div>" + '<br>';
+    var previous_msg = document.getElementById("recent_msg_" + data['receiver_id'] + "_" + data['sender_id'])
+
     if(news_area != null){
-      news_area.insertAdjacentHTML('beforeend', data['senderid'] + " からの新着メッセージがあります" + '<br>');
+      if(previous_msg == null){
+        news_area.insertAdjacentHTML('beforeend', content);
+      }
+      else{
+        content = "<div id='" + "recent_msg_" + data['receiver_id'] + "_" + data['sender_id'] + "'>" + data['senderid'] + " からの新着メッセージが複数件あります" + "</div>" + '<br>';
+        previous_msg.innerHTML = content;
+      }
     }
   },
 
