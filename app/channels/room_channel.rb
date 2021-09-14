51bbdaa170da74bd9356e666c2c08786999df3e2
tomohiro0827 <w40tomosub160.04@gmail.com>
2021-09-15 03:14:14 +0900

    r

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -14,8 +14,8 @@ class RoomChannel < ApplicationCable::Channel
     #ActionCable.server.broadcast 'room_channel', content: data['comment'], senderid: data['senderid'], receiverid: data['receiverid']
     logger = Logger.new('log.log')
     logger.debug(data)
-    Comment.create! content:data['content'], title:data['title'], senderid:data['senderid'], sender_id:data['sender_id'].to_i, receiver_id:data['receiver_id'].to_i, room_id:data['room_id'].to_i
-    RoomChannel.broadcast_to content:data['content'], sender_id:data['sender_id'].to_i, receiver_id:data['receiver_id'].to_i, room_id:data['room_id'].to_i
+    Comment.create! content:data['content'], title:data['title'], senderid:data['senderid'], sender_id:data['sender_id'].to_i, receiver_id:data['receiver_id'].to_i, room_id:data['room_id'].to_i, emotion:data['emotion'].to_i, expression:data['expression'].to_i
+    RoomChannel.broadcast_to content:data['content'], title:data['title'], senderid:data['senderid'], sender_id:data['sender_id'].to_i, receiver_id:data['receiver_id'].to_i, room_id:data['room_id'].to_i, emotion:data['emotion'].to_i, expression:data['expression'].to_i
     NewsChannel.broadcast_to content:data['content'], senderid:data['senderid'], sender_id:data['sender_id'].to_i, receiver_id:data['receiver_id'].to_i, room_id:data['room_id'].to_i
     #RoomChannel.broadcast_to data['content'], data['senderid'], data['receiverid']
   end
