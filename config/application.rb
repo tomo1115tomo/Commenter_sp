ca7e1da3de263b2f5cb2ba9083d2f33b3994e49a
tomohiro0827 <w40tomosub160.04@gmail.com>
2021-09-15 21:35:09 +0900

    v

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.action_view.automatically_disable_submit_tag = false
 
 module Commenter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.action_view.automatically_disable_submit_tag = false
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
 
