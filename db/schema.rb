328887b77e0eee4c45b894133e967f8363fbb5e9
ogawa <ogawa@ds.it.aoyama.ac.jp>
2022-11-15 14:36:52 +0900

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -47,12 +47,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_132910) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "roomid"
-    t.integer "receiver_id"
-    t.integer "sender_id"
-    t.integer "room_id"
-    t.integer "emotion"
-    t.integer "expression"
-    t.text "title"
     t.text "long_content"
   end
 
@@ -78,12 +72,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_132910) do
     t.integer "room_id"
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.string "new"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "userid"
@@ -91,8 +79,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_132910) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.boolean "admin", default: false, null: false
-    t.string "image"
-    t.text "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
